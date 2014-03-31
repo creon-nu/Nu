5e7a0338bffde20245a11b0b57f44b21cc58e741
fuzzybearbtc <peter@illustrate-archaeology.com>
2014-03-31 22:25:22 +0000

    Reduce system to 4 decimal place

--- src/util.h
+++ src/util.h
@@ -37,7 +37,7 @@ typedef int pid_t; /* define for windows compatiblity */
 typedef long long  int64;
 typedef unsigned long long  uint64;
 
-static const int64 COIN = 1000000;
+static const int64 COIN = 1000;
 static const int64 CENT = 10000;
 
 #define loop                for (;;)
