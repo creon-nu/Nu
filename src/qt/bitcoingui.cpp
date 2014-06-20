ae2d459ae463a7993c8af05182b8bc858036ac9d
sircoingame <sircoingame@gmail.com>
2014-06-19 21:59:21 -0400

    Branding modifications

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -75,7 +75,7 @@ BitcoinGUI::BitcoinGUI(QWidget *parent):
     rpcConsole(0)
 {
     resize(850, 550);
-    setWindowTitle(tr("Peercoin (PPCoin) Wallet"));
+    setWindowTitle(tr("Nu"));
 #ifndef Q_WS_MAC
     setWindowIcon(QIcon(":icons/ppcoin"));
 #else
