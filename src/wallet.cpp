e9071600bc73019b0168486c413512c43bc8e59d
Ben Rossi <ben@rossinet.com>
2014-08-14 12:02:02 -0400

    Add  RPC command to Nu [Refs #81]

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -12,6 +12,7 @@
 #include "bitcoinrpc.h"
 #include "script.h"
 #include "vote.h"
+#include <boost/algorithm/string/replace.hpp>
 
 using namespace std;
 
@@ -330,6 +331,14 @@ void CWallet::MarkDirty()
     }
 }
 
+static void
+runCommand(std::string strCommand)
+{
+    int nErr = ::system(strCommand.c_str());
+    if (nErr)
+        printf("runCommand error: system(%s) returned %d\n", strCommand.c_str(), nErr);
+}
+
 bool CWallet::AddToWallet(const CWalletTx& wtxIn)
 {
     uint256 hash = wtxIn.GetHash();
@@ -412,6 +421,16 @@ bool CWallet::AddToWallet(const CWalletTx& wtxIn)
 
         // since AddToWallet is called directly for self-originating transactions, check for consumption of own coins
         WalletUpdateSpent(wtx);
+
+        // notify an external script when a wallet transaction comes in or is updated
+        std::string strCmd = GetArg("-walletnotify", "");
+
+        if (!strCmd.empty())
+        {
+            boost::replace_all(strCmd, "%s", wtxIn.GetHash().GetHex());
+            boost::thread t(runCommand, strCmd); // thread runs free
+        }
+
     }
 
     // Refresh UI
