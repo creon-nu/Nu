94b045e6de080b0525bd8da834fb473b60102728
Michael Witrant <michael@ouvrages-web.fr>
2014-09-01 10:38:18 +0200

    Raise original exception on timeout

--- src/test/features/step_definitions/revert_duplicate.rb
+++ src/test/features/step_definitions/revert_duplicate.rb
@@ -7,13 +7,23 @@ end
 
 require 'timeout'
 def wait_for(timeout = 5)
-  Timeout.timeout(timeout) do
-    loop do
-      begin
-        break if yield
-      rescue RSpec::Expectations::ExpectationNotMetError
+  last_exception = nil
+  begin
+    Timeout.timeout(timeout) do
+      loop do
+        begin
+          break if yield
+        rescue RSpec::Expectations::ExpectationNotMetError => e
+          last_exception = e
+        end
+        sleep 0.1
       end
-      sleep 0.1
+    end
+  rescue Timeout::Error
+    if last_exception
+      raise last_exception
+    else
+      raise
     end
   end
 end
