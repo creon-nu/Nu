56637a088bb37b11edb2b2f5ba7e848e90561b40
Michael Witrant <michael@ouvrages-web.fr>
2015-03-08 09:01:32 +0100

    Null data output are not spendable

--- src/script.cpp
+++ src/script.cpp
@@ -1424,9 +1424,8 @@ int ScriptSigArgsExpected(txnouttype t, const std::vector<std::vector<unsigned c
     switch (t)
     {
     case TX_NONSTANDARD:
-        return -1;
     case TX_NULL_DATA:
-        return 1;
+        return -1;
     case TX_PUBKEY:
         return 1;
     case TX_PUBKEYHASH:
