1853ea8d17626a9080feedbb4ad9ee46f9decbea
John L. Jegutanis <erasmospunk@gmail.com>
2015-04-19 15:17:02 +0200

    improved unit tests for private key export format

--- src/test/key_tests.cpp
+++ src/test/key_tests.cpp
@@ -10,21 +10,25 @@
 
 using namespace std;
 
+// Mixed style keys
 static const string strDefaultSecret1     ("7QRR3XwvHW963ERsAQQi2JScqv2PgNDcrwMXwAcmJ3g9byEbRqN");
 static const string strDefaultSecret2     ("7RexmM8ERicLWvtNVZE3aQnViZLr2gqq1dShPDcSdmgf3MjNx64");
 static const string strDefaultSecret1C    ("VGNm8BbQBqchErGkQWKe4jz5Cu396kehHQudY5SrWEonyL9d9dhs");
 static const string strDefaultSecret2C    ("VMpZ8jpvG6ey96qjs3EaNTyokRkbQSrWumrRF4fS9JE2uPQ8a1xR");
 
+// NuShares keys
 static const string strSSecret1     ("5zn9do6aVdjaNn2F64LjxqG6P8AENnpqp6P1gsj7YXPrstEgQxL");
 static const string strSSecret2     ("621hMcGtdrCprUUkRDA5WwbyFmUgj7T3xnUB8vintFQNKHqSpda");
 static const string strSSecret1C    ("P42H7QTcqP8vK7iVBKk8dS1ckebk3BGASrkA2ztst99u9Rn9aEhP");
 static const string strSSecret2C    ("P9U57xh8ueBCDNHUdrf4wA1MJBKCLsTz5Dgwjz7TXCa95V2dt5c1");
 
+// NuBits keys
 static const string strBSecret1     ("62itp2YcM4QoWbbwzdxjv9rsJEmx2VfibjT7w2R1YBQFNWuRPef");
 static const string strBSecret2     ("63xSXqivVGt3zJ4TKnn5UGCkAt6QNpHvkRYHP5QgsuQkov3sKB2");
 static const string strBSecret1C    ("PCbugZpX1Y44HdjFjQ92Rc7715oPsbXtcJNHbTEyNcucRkyvVT4h");
 static const string strBSecret2C    ("PJ3hh8435o6LBtJFBw3xjL6qYcWrBHjiEfK5JSTZ1gKrMpGGxHs2");
 
+// The resulting NSR addresses for the above keys
 static const CBitcoinAddress addrS1 ("SkYqsCFMoSkgZh5gqzses71yRWZsvBTYTA");
 static const CBitcoinAddress addrS2 ("SbNy74rQ5yGkWw6LiagR8Nnch3TNyMGvd4");
 static const CBitcoinAddress addrS1C("Sj6Jtef2gkNXBW5MqxS8qUmsWJeewfKuan");
@@ -156,7 +160,7 @@ BOOST_AUTO_TEST_CASE(key_test1)
 
 BOOST_AUTO_TEST_CASE(key_test2)
 {
-    CBitcoinSecret bsecret1, bsecret2, bsecret1C, bsecret2C, baddress1;
+    CBitcoinSecret bsecret1, bsecret2, bsecret1C, bsecret2C;
     // New encoding for NuShares
     BOOST_CHECK( bsecret1.SetString (strSSecret1,  'S'));
     BOOST_CHECK( bsecret2.SetString (strSSecret2,  'S'));
@@ -179,10 +183,32 @@ BOOST_AUTO_TEST_CASE(key_test2)
     BOOST_CHECK(!bsecret1C.SetString(strBSecret1C, 'S'));
     BOOST_CHECK(!bsecret2C.SetString(strBSecret2C, 'S'));
 
+    // Old keys encoding for NuShares
+    BOOST_CHECK( bsecret1.SetString (strDefaultSecret1,  'S'));
+    BOOST_CHECK( bsecret2.SetString (strDefaultSecret2,  'S'));
+    BOOST_CHECK( bsecret1C.SetString(strDefaultSecret1C, 'S'));
+    BOOST_CHECK( bsecret2C.SetString(strDefaultSecret2C, 'S'));
+
+    // Old keys encoding for NuBits
+    BOOST_CHECK( bsecret1.SetString (strDefaultSecret1,  'B'));
+    BOOST_CHECK( bsecret2.SetString (strDefaultSecret2,  'B'));
+    BOOST_CHECK( bsecret1C.SetString(strDefaultSecret1C, 'B'));
+    BOOST_CHECK( bsecret2C.SetString(strDefaultSecret2C, 'B'));
+
     // Check the CBitcoinSecret
     bool fCompressed;
-    BOOST_CHECK_EQUAL(CBitcoinSecret(bsecret1.GetSecret(fCompressed), fCompressed, 'S').ToString(), strSSecret1);
-    BOOST_CHECK_EQUAL(CBitcoinSecret(bsecret1.GetSecret(fCompressed), fCompressed, 'B').ToString(), strBSecret1);
+    CSecret secret = bsecret1.GetSecret(fCompressed);
+    BOOST_CHECK(CBitcoinSecret(secret, fCompressed, 'S').ToString() == strSSecret1);
+    BOOST_CHECK(CBitcoinSecret(secret, fCompressed, 'B').ToString() == strBSecret1);
+    secret = bsecret2.GetSecret(fCompressed);
+    BOOST_CHECK(CBitcoinSecret(secret, fCompressed, 'S').ToString() == strSSecret2);
+    BOOST_CHECK(CBitcoinSecret(secret, fCompressed, 'B').ToString() == strBSecret2);
+    secret = bsecret1C.GetSecret(fCompressed);
+    BOOST_CHECK(CBitcoinSecret(secret, fCompressed, 'S').ToString() == strSSecret1C);
+    BOOST_CHECK(CBitcoinSecret(secret, fCompressed, 'B').ToString() == strBSecret1C);
+    secret = bsecret2C.GetSecret(fCompressed);
+    BOOST_CHECK(CBitcoinSecret(secret, fCompressed, 'S').ToString() == strSSecret2C);
+    BOOST_CHECK(CBitcoinSecret(secret, fCompressed, 'B').ToString() == strBSecret2C);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
